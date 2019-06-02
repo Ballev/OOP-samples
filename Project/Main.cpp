1b2e5b1d25e872b6f9d9055909bef01733e0c7f7
ABalev <sashobalev@abv.bg>
2019-06-02 20:20:17 +0300

    Create Main.cpp

---
+++ Project/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Animal.h"
+#include "Zoo.h"
+#include "Storage.h"
+#include "Constants.h"
+#include "Cage.h"
+#include "Execution.h"
+
+int main() {
+
+	start();
+  
+  return 0;
+}
