758b8a82689d14ae758dc8875a99a2bdae420c0a
ABalev <sashobalev@abv.bg>
2019-06-06 15:29:19 +0300

    Update Reptile.cpp

--- Project/Reptile.cpp
+++ Project/Reptile.cpp
@@ -1,23 +1,9 @@
 #include "Reptile.h"
 
 
-Reptile::Reptile() : Animal()
-{}
-
 Reptile::Reptile(const char * _name, AnimalEatingHabits _habits, FoodTypes _food, unsigned int _quantity, AnimalHabitat _habitat, const char * _sound)
 	: Animal(_name, REPTILE, _habits, _food, _quantity, _habitat, _sound)
 {}
-
-
-Reptile::Reptile(const Reptile & other) : Animal(other)
-{}
-
-Reptile & Reptile::operator=(const Reptile & other) {
-
-	Animal::operator=(other);
-
-	return *this;
-}
 
 Reptile::~Reptile()
 {}
@@ -27,4 +13,9 @@ void Reptile::print() {
 	std::cout << "Our animal: " << getName() << " is a " << eatingHabitsToString() << " reptile.\nIt eats " <<
 		foodToString() << ". It eats " << getFoodQuantity() << " kg of food every day.\n" <<
 		"The animal lives in " << habitatToString() << " and produces " << getSound() << " sound.\n";
+}
+
+Animal* Reptile::clone(){
+	
+	return new Reptile(*this);
 }
