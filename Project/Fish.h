825cf74219d3d542c5d88fbe412658e362851d7d
ABalev <sashobalev@abv.bg>
2019-06-06 15:25:34 +0300

    Update Fish.h

--- Project/Fish.h
+++ Project/Fish.h
@@ -7,14 +7,12 @@ class Fish
 	: public Animal {
 
 public:
-	Fish();
 	Fish(const char*, AnimalEatingHabits, FoodTypes, unsigned int, AnimalHabitat, const char*);
-	Fish(const Fish&);
-	Fish& operator=(const Fish&);
 	~Fish();
 
 public:
 	virtual void print() override;
+	virtual Animal* clone() override;
 };
 
 #endif  // !__FISH_HEADER__
