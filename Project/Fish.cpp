f16fb80445b790881ad3629f520a74c3f54f36b7
ABalev <sashobalev@abv.bg>
2019-06-06 15:31:25 +0300

    Update Fish.cpp

--- Project/Fish.cpp
+++ Project/Fish.cpp
@@ -1,23 +1,9 @@
 #include "Fish.h"
 
 
-Fish::Fish() : Animal()
-{}
-
 Fish::Fish(const char * _name, AnimalEatingHabits _habits, FoodTypes _food, unsigned int _quantity, AnimalHabitat _habitat, const char * _sound)
 	: Animal(_name, FISHES, _habits, _food, _quantity, _habitat, _sound)
 {}
-
-
-Fish::Fish(const Fish & other) : Animal(other)
-{}
-
-Fish & Fish::operator=(const Fish & other) {
-
-	Animal::operator=(other);
-
-	return *this;
-}
 
 Fish::~Fish()
 {}
@@ -27,4 +13,9 @@ void Fish::print() {
 	std::cout << "Our animal: " << getName() << " is a " << eatingHabitsToString() << " fish.\nIt eats " <<
 		foodToString() << ". It eats " << getFoodQuantity() << " kg of food every day.\n" <<
 		"The animal lives in " << habitatToString() << " and produces " << getSound() << " sound.\n";
+}
+
+Animal* Fish::clone(){
+	
+	return new Fish(*this);
 }
