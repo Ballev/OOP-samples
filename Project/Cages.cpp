44b578e98c2194c7efbb29f3493e585deb68c681
ABalev <sashobalev@abv.bg>
2019-06-06 15:36:56 +0300

    Update Cages.cpp

--- Project/Cages.cpp
+++ Project/Cages.cpp
@@ -1,18 +1,9 @@
 #include "Cages.h"
 
+
 Forest::Forest(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage("_name", 45, 85, FORESTY, _location, _eatsMeat, FOREST)
 {}
-
-Forest::Forest(const Forest & other) : Cage(other)
-{}
-
-Forest & Forest::operator=(const Forest & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-}
 
 Forest::~Forest()
 {}
@@ -23,23 +14,12 @@ void Forest::print() {
 		<< "% and the flora around in the cage is " << getFlora() << "%. The cage's terrain is " << terrainToString()
 		<< ".\n The cage is located on " << getLocation() << " and is used for animals that " << isForMeatEatersToString()
 		<< std::endl << std::endl;
-
 }
 
 
 Savanna::Savanna(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage(_name, 30, 50, HILLS, _location, _eatsMeat, SAVANNA)
 {}
-
-Savanna::Savanna(const Savanna & other) : Cage(other)
-{}
-
-Savanna & Savanna::operator=(const Savanna & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-}
 
 Savanna::~Savanna()
 {}
@@ -57,16 +37,6 @@ Jungle::Jungle(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage(_name, 76, 98, FORESTY, _location, _eatsMeat, JUNGLE)
 {}
 
-Jungle::Jungle(const Jungle & other) : Cage(other)
-{}
-
-Jungle & Jungle::operator=(const Jungle & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-}
-
 Jungle::~Jungle()
 {}
 
@@ -82,16 +52,6 @@ void Jungle::print() {
 ArcticZone::ArcticZone(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage(_name, 55, 0, WATER, _location, _eatsMeat, ARCTICZONE)
 {}
-
-ArcticZone::ArcticZone(const ArcticZone & other) : Cage(other)
-{}
-
-ArcticZone & ArcticZone::operator=(const ArcticZone & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-}
 
 ArcticZone::~ArcticZone()
 {}
@@ -109,16 +69,6 @@ Aquarium::Aquarium(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage(_name, 100, 25, WATER, _location, _eatsMeat, AQUARIUM)
 {}
 
-Aquarium::Aquarium(const Aquarium & other) : Cage(other)
-{}
-
-Aquarium & Aquarium::operator=(const Aquarium & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-}
-
 Aquarium::~Aquarium()
 {}
 
@@ -134,17 +84,6 @@ void Aquarium::print() {
 Terrarium::Terrarium(const char* _name, const char * _location, bool _eatsMeat)
 	: Cage(_name, 55, 30, HILLS, _location, _eatsMeat, TERRARIUM)
 {}
-
-Terrarium::Terrarium(const Terrarium & other) : Cage(other)
-{}
-
-Terrarium & Terrarium::operator=(const Terrarium & other) {
-
-	Cage::operator=(other);
-
-	return *this;
-
-}
 
 Terrarium::~Terrarium()
 {}
